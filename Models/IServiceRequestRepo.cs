c460873c5d3482ba3bc7df571ef73d62587751e0
Roger Carter <rogerjcarter@yahoo.com>
2020-07-12 18:19:26 -0500

    Update IServiceRequestRepo.cs

--- Models/IServiceRequestRepo.cs
+++ Models/IServiceRequestRepo.cs
@@ -1,1 +1,21 @@
 
+using System;
+using System.Linq;
+
+using System.Collections.Generic;
+namespace CohesionCodingDemo.Models
+{
+    public interface IServiceRequestRepo
+    {
+        public GetServiceRequest(string Id);
+
+        public List<ServiceRequest> GetServiceRequests();
+
+        public ServiceRequest AddServiceRequest(ServiceRequest Id);
+
+        public ServiceRequest UpdateServiceRequest(string Id, ServiceRequest ServiceRequestItem);
+
+        public string DeleteServiceRequest(string Id);
+
+    }
+}
